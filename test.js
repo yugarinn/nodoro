6feae71ca3747a8415240e8bfd6915fd32b4160f
Sergio Uve <sergiouve@gmail.com>
2020-09-21 21:32:20 +0200

    Add new statuses logic

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
+const readline = require('readline')
+const { EOL } = require('os')
 const Timer = require('./lib/Timer')
 const timer = new Timer()
-const { EOL } = require('os')
 
-let data = null
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+})
 
 timer.reset()
 timer.start()
@@ -10,32 +15,18 @@ timer.start()
 const loop = () => {
   process.stdout.write('\x1Bc')
   console.log(timer.status())
-  console.log('data: ', data)
 }
 
 setInterval(loop, 1000)
 
-process.stdin.setEncoding('utf8');
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read()
+rl.on('line', (input) => {
+  if (input == 's' && timer.getStatus() == 'RUNNING') {
+    timer.stop()
+  } else if (input == 's' && timer.getStatus() == 'STOPPED') {
+    timer.start()
+  }
 
-  chunk
-    .split(EOL)
-    .forEach((input) => {
-      if (input === null || !input.length) {
-        return
-      }
-
-      data = input
-
-      if (input == 's' && timer.getStatus() == 'RUNNING') {
-        timer.stop()
-      } else if (input == 's' && timer.getStatus() == 'STOPPED') {
-        timer.start()
-      }
-
-      if (input == 'r') {
-        timer.reset()
-      }
-    })
+  if (input == 'r') {
+    timer.reset()
+  }
 })
