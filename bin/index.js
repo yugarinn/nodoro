58130b9a755f2e8880c5f8d8c7cdee0eb7761a04
Sergio Uve <sergiouve@gmail.com>
2020-09-23 01:10:41 +0200

    Add proper remaining time calc and controls

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,37 @@
 #! /usr/bin/env node
 
+const { EOL } = require('os')
 const Timer = require('../lib/Timer')
-const Ui = require('../lib/Ui')
-
 const timer = new Timer()
-const ui = new Ui(timer)
 
-ui.boot()
+const stdin = process.openStdin()
+stdin.setRawMode(true)
+stdin.resume();
+stdin.setEncoding( 'utf8' );
+
+timer.reset()
+timer.start()
+
+const loop = () => {
+  process.stdout.write('\x1Bc')
+  process.stdout.write(timer.status())
+  process.stdout.write(`${EOL}${EOL}s: pause/resume | r: restart`)
+}
+
+stdin.on('data', (input) => {
+  if (input === '\u0003') {
+    process.exit();
+  }
+
+  if (input === '\u0073' && timer.getStatus() == 'RUNNING') {
+    timer.stop()
+  } else if (input === '\u0073' && ['STOPPED', 'CLEAN'].includes(timer.getStatus())) {
+    timer.start()
+  }
+
+  if (input === '\u0072') {
+    timer.reset()
+  }
+})
+
+setInterval(loop, 100)
