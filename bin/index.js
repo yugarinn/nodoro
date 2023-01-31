8097ff2e4c3af1af80f501bd28d1bbbdca162b02
Sergio Uve <sergiouve@gmail.com>
2023-01-31 21:35:27 +0100

    Refactor and shit

--- bin/index.js
+++ bin/index.js
@@ -3,8 +3,8 @@
 const { EOL } = require('os')
 
 const config = require('../lib/config')()
-const Logger = require('../lib/Logger')
-const Timer = require('../lib/Timer')
+const { stateToHuman, remainingToHuman, noiserStateToHuman } = require('../lib/printers')
+const Ticker = require('../lib/Ticker')
 const Noiser = require('../lib/Noiser')
 
 const sButton = '\u0073'
@@ -12,54 +12,31 @@ const rButton = '\u0072'
 const mButton = '\u006D'
 const ctrlCButton = '\u0003'
 
-const logger = new Logger()
-const timer = new Timer(logger)
+const ticker = new Ticker()
 const noiser = new Noiser()
 const stdin = process.openStdin()
 
-let timerAndNoiserAreBinded = false
-
 stdin.setRawMode(true)
-stdin.resume();
-stdin.setEncoding( 'utf8' );
+stdin.resume()
+stdin.setEncoding('utf8')
 
-timer.reset()
-timer.start()
+if (config.autoplay) noiser.play()
 
-if (config.autoplay) {
-  noiser.toggle()
-  timerAndNoiserAreBinded = true
-}
+const printTickerState = (ticker) => `${stateToHuman(ticker)} - ${remainingToHuman(ticker)} minutes left`
 
 const loop = () => {
-  const muteString = noiser.isRunning() ? 'mute' : 'unmute'
+  ticker.tick()
 
   process.stdout.write('\x1Bc')
-  process.stdout.write(timer.tick())
-  process.stdout.write(`${EOL}${EOL}s: pause/resume | r: restart | m: ${muteString} | Ctrl-c: exit`)
+  process.stdout.write(printTickerState(ticker))
+  process.stdout.write(`${EOL}${EOL}s: pause/resume | r: restart | m: ${noiserStateToHuman(noiser)} | Ctrl-c: exit`)
 }
 
 stdin.on('data', (input) => {
-  if (input === ctrlCButton) {
-    logger.clean()
-    process.exit();
-  }
-
-  if (input === sButton && timer.getState() === 'RUNNING') {
-    timer.stop()
-    if (timerAndNoiserAreBinded) noiser.stop()
-  } else if (input === sButton && timer.getState() === 'STOPPED') {
-    timer.start()
-    if (timerAndNoiserAreBinded) noiser.play()
-  }
-
-  if (input === rButton) {
-    timer.reset()
-  }
-
-  if (input === mButton) {
-    noiser.toggle()
-  }
+  if (input === sButton) ticker.toggle()
+  if (input === mButton) noiser.toggle()
+  if (input === rButton) ticker.reset()
+  if (input === ctrlCButton) process.exit()
 })
 
-setInterval(loop, 200)
+setInterval(loop, 1000)
